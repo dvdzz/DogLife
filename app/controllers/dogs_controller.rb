fef2280b9e0dc8a122268231b45087631fc43fb2
dvdzz <dvdzz76@icloud.com>
2019-09-19 21:39:25 +0000

    added a new dog form

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,4 +2,8 @@ class DogsController < ApplicationController
     def index
         @dogs = Dog.order(:id).paginate(:page => params[:page], :per_page => 5) 
     end
+
+    def new
+        @dog = Dog.new
+    end
 end
