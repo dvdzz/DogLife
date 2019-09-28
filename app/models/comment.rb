de09955c7cce3f97a96b92e03ac983ea53c61bd4
dvdzz <dvdzz76@icloud.com>
2019-09-28 20:36:34 +0000

    added a drop down dog rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,19 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :dog
+  
+    RATINGS = {
+      'one dog': '1_star',
+      'two dogs': '2_stars',
+      'three dogs': '3_stars',
+      'four dogs': '4_stars',
+      'five dogs': '5_stars'
+    }
+
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
+
+
 
 end
